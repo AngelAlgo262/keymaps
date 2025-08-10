876e2785c10ce72e4e372c292d87085e6d6b9c7e
Angel Ocampo <angelocampo262@gmail.com>
2025-08-09 21:48:33 -0600

    funciones

--- bm40v2/keymap.c
+++ bm40v2/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-    KC_LCTL, KC_PEQL, KC_LGUI, KC_LALT, LOWER,       KC_SPC,       RAISE,   KC_DEL,  KC_RCTL, KC_HOME, KC_END
+    KC_LCTL, KC_MENU, KC_LGUI, KC_LALT, LOWER,       KC_SPC,       RAISE,   KC_DEL,  KC_RCTL, KC_HOME, KC_END
 ),
 
 /* Lower
@@ -54,32 +54,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------|
  * | ~    |   !  |   @  |   {  |   }  |  &   |  NA  |  NA  |  NA  |  NA  |  NA  | NA   |
  * |-----------------------------------------------------------------------------------|
- * |      |   _  |   <  |   >  |Lower |    Space    |Raise |  NA  |  NA  |  NA  | NA   |
+ * | NA   |   _  |   <  |   >  |Lower |    Space    |Raise |  NA  |  NA  |  NA  | NA   |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    KC_PIPE, KC_PERC, KC_BSLS, KC_LBRC, KC_RBRC, KC_GRV,  NA,      NA,      KC_UP,   NA,      NA,      KC_BSPC,
-    KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, KC_PSCR, NA,      KC_LEFT, KC_DOWN, KC_RGHT, NA,      KC_ASTR,
-    KC_TILD, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR, NA,      NA,      NA,      NA,      NA,      NA,
-    NA,      KC_UNDS, KC_LT,   KC_GT,   _______, _______,_______,  NA,      NA,      NA,      NA,      NA
+    KC_PIPE, KC_PERC, KC_BSLS, KC_LBRC, KC_RBRC, KC_GRV,  NA,      NA,       KC_UP,   NA,      NA,      KC_BSPC,
+    KC_HASH, KC_EQL,  KC_DLR,  KC_LPRN, KC_RPRN, KC_PSCR, NA,      KC_LEFT,  KC_DOWN, KC_RGHT, NA,      KC_ASTR,
+    KC_TILD, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR, NA,      NA,       NA,      NA,      NA,      NA,
+    NA,      KC_UNDS, KC_LT,   KC_GT,   _______,    _______,       _______,  NA,      NA,      NA,      NA,
 )
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |  NA  |  NA  |  F1  |  F4  |  F11 |PagDwn|PagUp |  1!  |  2@  |  3#  |  +   | Bksp |
  * |-----------------------------------------------------------------------------------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |Shift |  NA  |  F2  |  F5  |  F12 |  NA  |  NA  |  4$  |  5%  |  6^  |  *   |Enter |
  * |-----------------------------------------------------------------------------------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |  NA  |  NA  |  F3  |  F9  |  NA  |  NA  |  NA  |  7&  |  8*  |  9(  |  /   |  =   |
  * |-----------------------------------------------------------------------------------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol- | Play |
+ * |Caps  |  NA  |  NA  |  NA  |Lower |   Space     |Raise |  0)  |  .>  | Del  |  -   |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_planck_mit(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+[_RAISE] = LAYOUT_ortho_4x12(
+    NA,      NA,    KC_F1,   KC_F4,   KC_F11,  KC_PGUP, KC_PGDN, KC_1,    KC_2,    KC_3,    KC_PLUS, KC_BSPC,
+    KC_LSFT, NA,    KC_F2,   KC_F5,   KC_F12,  NA,      NA,      KC_4,    KC_5,    KC_6,    KC_ASTR, KC_ENT,
+    NA,      NA,    KC_F3,   KC_F9,   NA,      NA,      NA,      KC_7,    KC_8,    KC_9,    KC_SLSH, KC_EQL,
+    KC_CAPS, NA,    NA,      NA,      _______,    _______,       _______, KC_0,    KC_DOT,  KC_DEL,  KC_MINS
 ),
 
 /* Adjust (Lower - Raise)
